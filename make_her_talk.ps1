e93398367c5df60eeaa9ad22fe5c9f4ee9298c08
Nathan Herling <nth@arizona.edu>
2025-09-08 04:28:56 -0700

    Hazel works

---
+++ make_her_talk.ps1
@@ -0,0 +1,8 @@
+﻿Add-Type -AssemblyName System.Speech
+
+$synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+# Explicitly select Hazel voice
+$synthesizer.SelectVoice("Microsoft Hazel Desktop")
+
+$synthesizer.Speak("Hello! This is Microsoft Hazel, the British female voice. Testing, testing, one, two, three.")
